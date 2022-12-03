ff428272de0d81890644fc1c68f64abbf7630d55
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-03 19:33:57 +0500

    Weekend with switch case

---
+++ Task15/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число, обозначающее день недели (от 1 - понедельник, до 7 - воскресение):");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 0 && day < 8)
+{
+    switch(day)
+    {
+    case  1 :
+        Console.WriteLine("Это понедельник. Рабочий день.");
+        break;
+ 
+    case 2:
+        Console.WriteLine("Это вторник. Рабочий день.");
+        break;
+    case 3:
+        Console.WriteLine("Это среда. Рабочий день.");
+        break;
+    case 4:
+        Console.WriteLine("Это четверг. Рабочий день.");
+        break;
+    case 5:
+        Console.WriteLine("Это пятница. Рабочий день.");
+        break;
+    case 6:
+        Console.WriteLine("Это суббота. Ура! Выходной!");
+        break;
+    case 7:
+        Console.WriteLine("Это воскресение. Ура! Выходной!");
+        break;
+    }
+}
+else
+{
+    Console.WriteLine("В неделе нет такого дня.");
+}
