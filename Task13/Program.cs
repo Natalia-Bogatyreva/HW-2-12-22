ea3078c29ece3c95ed7a35c0c75e769bec2af456
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-03 19:16:43 +0500

    The third digit and checking for characters

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число, чтобы узнать какая цифра в нем - третья:");
+string str = Console.ReadLine();
+int i = str.Length;
+int num;
+bool Num;
+
+if (Num = Int32.TryParse(str, out num))
+{
+    if (i > 2)
+    {
+        Console.WriteLine("Третья цифра числа - > " + str[2]);
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели число, в котором нет третьей цифры.");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не число, а буквенные символы.");
+}
+
